f2d1d603b276327fef0209903337bd2d94ea25af
muhammed-udaif <muhammed.udaif@vonnue.com>
2026-01-05 11:31:07 +0530

    feat: add some packages and the button components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,31 @@
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
 import { View, StyleSheet} from "react-native";
 
 const placeholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imagecontainer}>
         <ImageViewer imgSource={placeholderImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo"/>
+        <Button theme="primary" label="Choose a photo"/>
         <Button label="Use this photo"/>
       </View>
     </View>
